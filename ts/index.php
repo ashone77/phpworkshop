c86b3a7c433a98d7b070c61e59e0916b3f2092c5
Aswin Haridas <aswinvharidas@gmail.com>
2021-12-20 21:36:25 +0530

    ts folder added

---
+++ ts/index.php
@@ -0,0 +1,17 @@
+<?php 
+include('./config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teachers Login</title>
+</head>
+<body>
+    <h1>Teachers Login</h1>
+    <form action=""></form>
+</body>
+</html>
